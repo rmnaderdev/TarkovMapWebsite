b2fed1746c5332e8fd3054a240f97710684816b9
Richard Nader, Jr. <rmnaderdev@gmail.com>
2024-06-29 02:22:10 -0400

    WIP: Sidebar toggle.

--- TarkovMapWebsite/ClientApp/src/App.vue
+++ TarkovMapWebsite/ClientApp/src/App.vue
@@ -1,30 +1,11 @@
 <script setup lang="ts">
 import {RouterView} from "vue-router";
-import { IconHome, IconMap } from '@tabler/icons-vue';
-import {Maps} from "./maps.ts";
+import NavBar from "./components/NavBar.vue";
 </script>
 
 <template>
   <div class="w-full max-h-screen flex flex-col sm:flex-row flex-wrap sm:flex-nowrap py-4 grow">
-    <div class="w-fixed w-full flex-shrink flex-grow-0 px-4 ">
-      <div class="sticky top-0 p-4 bg-gray-100 rounded-xl w-full h-full">
-        <ul class="flex sm:flex-col overflow-hidden content-center justify-center gap-2">
-          <li>
-            <RouterLink class="flex py-2 hover:bg-blue-400 rounded" exact-active-class="bg-blue-400" to="/">
-              <IconHome class="w-7 sm:mx-2 mx-4 inline" stroke={2} /> <span class="hidden sm:inline">Home</span>
-            </RouterLink>
-          </li>
-
-          <li v-for="map in Maps" :key="map.name">
-            <RouterLink class="flex py-2 hover:bg-blue-400 rounded" exact-active-class="bg-blue-400" :to="map.link">
-              <IconMap class="w-7 sm:mx-2 mx-4 inline" stroke={2} /> <span class="hidden sm:inline">{{ map.navLinkName || map.name }}</span>
-            </RouterLink>
-          </li>
-          
-        </ul>
-      </div>
-    </div>
-
+    <NavBar />
     <main class="w-full flex-shrink flex-grow-0 flex flex-col pt-1 px-3">
       <RouterView/>
     </main>
@@ -32,29 +13,5 @@ import {Maps} from "./maps.ts";
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-
-.router-link-active {
-  font-weight: bold;
-}
-
-a {
-  font-weight: 500;
-  color: #242424;
-  text-decoration: inherit;
-}
 
 </style>
---
+++ TarkovMapWebsite/ClientApp/src/components/NavBar.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import { IconHome, IconMap } from '@tabler/icons-vue';
+import {Maps} from "../maps.ts";
+import {ref} from "vue";
+
+// Create sidebar open/close functionality
+const sidebar = ref(false);
+const toggleSidebar = () => {
+  sidebar.value = !sidebar.value;
+};
+
+
+</script>
+
+<template>
+  <div class="w-fixed w-full flex-shrink flex-grow-0 px-4">
+    <div class="sticky top-0 p-4 bg-gray-100 rounded-xl w-full h-full">
+      <ul class="flex sm:flex-col overflow-hidden content-center justify-center gap-2">
+        <li>
+          <RouterLink class="flex py-2 hover:bg-blue-400 rounded" exact-active-class="bg-blue-400" to="/">
+            <IconHome class="w-7 sm:mx-2 mx-4 inline" stroke={2} /> <span class="hidden sm:inline">Home</span>
+          </RouterLink>
+        </li>
+
+        <li v-for="map in Maps" :key="map.name">
+          <RouterLink class="flex py-2 hover:bg-blue-400 rounded" exact-active-class="bg-blue-400" :to="map.link">
+            <IconMap class="w-7 sm:mx-2 mx-4 inline" stroke={2} /> <span class="hidden sm:inline">{{ map.navLinkName || map.name }}</span>
+          </RouterLink>
+        </li>
+
+      </ul>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  .router-link-active {
+    font-weight: bold;
+  }
+  
+  a {
+    font-weight: 500;
+    color: #242424;
+    text-decoration: inherit;
+  }
+</style>
